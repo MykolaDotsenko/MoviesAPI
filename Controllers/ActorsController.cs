a596620752e85648b88b12bd4df639e07f0524b5
Mykola Dotsenko <docnikolaj1990@gmail.com>
2025-03-30 23:26:46 +0300

    Creating Actors

--- Controllers/ActorsController.cs
+++ Controllers/ActorsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OutputCaching;
 using MoviesAPI.DTOs;
 using MoviesAPI.Entities;
+using MoviesAPI.Services;
 
 namespace MoviesAPI.Controllers
 {
@@ -13,20 +14,29 @@ namespace MoviesAPI.Controllers
         private readonly ApplicationDbContext context;
         private readonly IMapper mapper;
         private readonly IOutputCacheStore outputCacheStore;
+        private readonly IFileStorage fileStorage;
         private const string cacheTag = "actors";
+        private readonly string container = "actors";
 
-        public ActorsController(ApplicationDbContext context, IMapper mapper, IOutputCacheStore outputCacheStore) {
+        public ActorsController(ApplicationDbContext context, IMapper mapper, IOutputCacheStore outputCacheStore, IFileStorage fileStorage)
+        {
             this.context = context;
             this.mapper = mapper;
             this.outputCacheStore = outputCacheStore;
+            this.fileStorage = fileStorage;
         }
 
         [HttpPost]
         public async Task<IActionResult> Post([FromForm] ActorCreationDTO actorCreationDTO)
         {
             var actor = mapper.Map<Actor>(actorCreationDTO);
-            // TODO: Work wth the picture
-            context.Add(actor);
+
+if(actorCreationDTO.Picture is not null)
+            {
+                var url = await fileStorage.Store(container, actorCreationDTO.Picture);
+            }
+
+                context.Add(actor);
             await context.SaveChangesAsync();
             await outputCacheStore.EvictByTagAsync(cacheTag, default);
             return Ok();
