d60c311bd17df9b3ee5002f8232ef6e7e6b46493
Mykola Dotsenko <docnikolaj1990@gmail.com>
2025-03-30 21:52:43 +0300

    Creating Actors -

---
+++ Controllers/ActorsController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+using MoviesAPI.DTOs;
+using MoviesAPI.Entities;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/actors")]
+    [ApiController]
+    public class ActorsController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+        private readonly IOutputCacheStore outputCacheStore;
+        private const string cacheTag = "actors";
+
+        public ActorsController(ApplicationDbContext context, IMapper mapper, IOutputCacheStore outputCacheStore) {
+            this.context = context;
+            this.mapper = mapper;
+            this.outputCacheStore = outputCacheStore;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] ActorCreationDTO actorCreationDTO)
+        {
+            var actor = mapper.Map<Actor>(actorCreationDTO);
+            // TODO: Work wth the picture
+            context.Add(actor);
+            await context.SaveChangesAsync();
+            await outputCacheStore.EvictByTagAsync(cacheTag, default);
+            return Ok();
+        }
+    }
+}
