88ce59eed567933303bc66232d17443a86ac8711
Mykola Dotsenko <docnikolaj1990@gmail.com>
2025-03-30 19:46:23 +0300

    Deleting Genres

--- Controllers/GenresController.cs
+++ Controllers/GenresController.cs
@@ -90,9 +90,18 @@ namespace MoviesAPI.Controllers
 
         }
 
-        [HttpDelete]
-        public void Delete()
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
         {
+            var deletedRecords = await context.Genres.Where(g => g.Id == id).ExecuteDeleteAsync();
+
+            if (deletedRecords == 0)
+            {
+                return NotFound();
+            }
+
+            await outputCacheStore.EvictByTagAsync(cacheTag, default);
+            return NoContent();
 
         }
     }
