b70ba36bd970d8699f19149f7d820129a04ee62e
Mykola Dotsenko <docnikolaj1990@gmail.com>
2025-04-05 22:52:40 +0300

    Custom Base Controller - Creating, Updating and Deleting

--- Controllers/CustomBaseController.cs
+++ Controllers/CustomBaseController.cs
@@ -2,8 +2,10 @@
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
 using Microsoft.EntityFrameworkCore;
 using MoviesAPI.DTOs;
+using MoviesAPI.Entities;
 using MoviesAPI.Utilities;
 
 namespace MoviesAPI.Controllers
@@ -12,10 +14,15 @@ namespace MoviesAPI.Controllers
     {
         private readonly ApplicationDbContext context;
         private readonly IMapper mapper;
+        private readonly IOutputCacheStore outputCacheStore;
+        private readonly string cacheTag;
 
-        public CustomBaseController(ApplicationDbContext context, IMapper mapper) {
+        public CustomBaseController(ApplicationDbContext context, IMapper mapper,
+            IOutputCacheStore outputCacheStore, string cacheTag) {
             this.context = context;
             this.mapper = mapper;
+            this.outputCacheStore = outputCacheStore;
+            this.cacheTag = cacheTag;
         }
       protected async Task<List<TDTO>> Get<TEntity, TDTO>(PaginationDTO pagination,
           Expression<Func<TEntity, object>> orderBy)
@@ -42,6 +49,60 @@ namespace MoviesAPI.Controllers
                 return NotFound();
             }
             return entity;
+        }
+
+        protected async Task<CreatedAtRouteResult> Post<TCreation, TEntity, TRead>(TCreation creationDTO,
+            string routeName)
+            where TEntity : class
+            where TRead : IId
+        {
+            var entity = mapper.Map<TEntity>(creationDTO);
+            context.Add(entity);
+            await context.SaveChangesAsync();
+            await outputCacheStore.EvictByTagAsync(cacheTag, default);
+            var entityDTO = mapper.Map<TRead>(entity);
+
+
+            return CreatedAtRoute(routeName, new { id = entityDTO.Id }, entityDTO);
+        }
+
+
+        protected async Task<IActionResult> Put<TCreation, TEntity>(int id, TCreation creationDTO)
+            where TEntity: class, IId
+        {
+            var entityExists = await context.Set<TEntity>().AnyAsync(e => e.Id == id);
+
+            if (!entityExists)
+            {
+                return NotFound();
+            }
+            var entity = mapper.Map<TEntity>(creationDTO);
+            entity.Id = id;
+            context.Update(entity);
+            await context.SaveChangesAsync();
+            await outputCacheStore.EvictByTagAsync(cacheTag, default);
+
+            return NoContent();
+        }
+
+
+        protected async Task<IActionResult> Delete<TEntity>(int id)
+            where TEntity : class, IId
+        {
+
+
+
+
+
+            var deletedRecords = await context.Set<TEntity>().Where(g => g.Id == id).ExecuteDeleteAsync();
+
+            if (deletedRecords == 0)
+            {
+                return NotFound();
+            }
+
+            await outputCacheStore.EvictByTagAsync(cacheTag, default);
+            return NoContent();
         }
     }
 }
